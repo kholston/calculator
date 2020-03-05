e8275c4e02ed60b1b9e15d878017f239206565c2
Ken Holston <git.kholston@gmail.com>
2020-03-05 10:04:08 -0500

    Add operate function

--- js/main.js
+++ js/main.js
@@ -17,3 +17,31 @@ function divide(num1,num2)
 {
     return num1 / num2;
 }
+
+function operate(num1,num2,operator)
+{
+    let result = 0;
+    switch(operator){
+        case add:
+            result = add(num1,num2);
+            break;
+
+        case subtract:
+            result = subtract(num1,num2);
+            break;
+
+        case multiply:
+            result = multiply(num1,num2);
+            break;
+
+        case divide:
+            result = divide(num1,num2);
+            break; 
+
+        default:
+            result = "ERR";           
+        }    
+    
+
+    return result;
+}
